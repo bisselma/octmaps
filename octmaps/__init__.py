becc741eb665f1d6adf00f6cde1209751b59a38b
Ben Isselmann <49669378+bisselma@users.noreply.github.com>
2022-01-31 19:13:10 +0100

    Delete octmaps directory

--- octmaps/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Top-level package for eyepy."""
-
-__author__ = """Olivier Morelle"""
-__email__ = "oli4morelle@gmail.com"
-__version__ = "0.3.6"
-
-from octmaps.core import LayerMaps, OctMaps
