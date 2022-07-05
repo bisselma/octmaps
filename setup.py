a2f2056ff7f89b184c4bada3205aef448cd14e4b
Ben Isselmann <ben.isselmann@gmail.com>
2022-07-05 11:40:56 +0200

    setup.py version: 0.2.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 __author__ = """Ben Isselmann"""
 __email__ = "ben.isselmann@gmail.com"
-__version__ = "0.2.9"
+__version__ = "0.2.8"
 
 
 #with open("README.rst") as readme_file:
@@ -15,7 +15,7 @@ __version__ = "0.2.9"
 #with open("HISTORY.rst") as history_file:
  #   history = history_file.read()
 
-requirements = ["eyepie==0.3.7", "'numpy>=1.22", "opencv-python", "opencv-python-headless", "Pillow"]
+requirements = ["eyepie==0.3.7", "numpy>=1.22", "opencv-python", "opencv-python-headless", "Pillow"]
 
 setup_requirements = ["pytest-runner"]
 
